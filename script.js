cb88aa4fa43ccead8551810d04c517ab0603d331
Douglas <douglinhauniverso@gmail.com>
2022-01-20 14:15:55 -0300

    Antes tarde do que nunca

---
+++ script.js
@@ -0,0 +1,33 @@
+// Crie uma variável que receba um nome;
+let nome = "DOUGLAS"
+console.log(nome)
+
+// Crie uma variável que receba um número;
+let number = 15
+console.log(number)
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+let ano = 2022
+console.log(ano - number)
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+
+console.log( ano + number)
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+
+let mensagem = "Estou mexendo no console.log"
+console.log(mensagem)
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+
+let quarta_feira = true
+
+if(quarta_feira == true){
+    console.log(quarta_feira)
+}
+
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log("boolean")
